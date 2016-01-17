6af27adbdbe8c3274d966c361a2811356ba0fb20
René Jeschke <rene_jeschke@yahoo.de>
2016-01-17 15:15:55 +0100

    Removed concurrency check TODO, no issue

--- src/org/adblockplus/sbrowser/contentblocker/engine/Subscriptions.java
+++ src/org/adblockplus/sbrowser/contentblocker/engine/Subscriptions.java
@@ -170,7 +170,6 @@ final class Subscriptions
    */
   private boolean writeFile(final File output) throws IOException
   {
-    // TODO verify possible concurrency issues with downloading
     final HashSet<String> filters = new HashSet<String>();
     for (final Subscription s : this.subscriptions.values())
     {
