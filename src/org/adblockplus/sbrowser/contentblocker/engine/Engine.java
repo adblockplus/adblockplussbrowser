4d3d164caa0c0751a6defa645304a5bd1c9858e3
René Jeschke <rene_jeschke@yahoo.de>
2016-01-17 15:32:50 +0100

    Removed needless import

--- src/org/adblockplus/sbrowser/contentblocker/engine/Engine.java
+++ src/org/adblockplus/sbrowser/contentblocker/engine/Engine.java
@@ -38,7 +38,6 @@ import org.adblockplus.adblockplussbrowser.R;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
