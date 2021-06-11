9f0777a656a20cac84f5860b3858e188919d3676
Jens Rösler <j.roesler@eyeo.com>
2021-06-11 09:15:24 +0200

    ABB-32 - About

---
+++ preferences/src/main/kotlin/org/adblockplus/adblockplussbrowser/preferences/ui/about/AboutViewModel.kt
@@ -0,0 +1,8 @@
+package org.adblockplus.adblockplussbrowser.preferences.ui.about
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+internal class AboutViewModel @Inject constructor() : ViewModel()
