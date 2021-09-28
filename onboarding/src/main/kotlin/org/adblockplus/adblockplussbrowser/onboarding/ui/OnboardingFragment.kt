b424770e70ef825c8fa0941b9c49f95d56e395bf
Jacek Tymicki <j.tymicki@eyeo.com>
2021-09-28 17:31:41 +0200

    DPC-534 - Sending Intent to Samsung browser settings
    Delay starting of Samsung Browser settings by 500 ms

--- onboarding/src/main/kotlin/org/adblockplus/adblockplussbrowser/onboarding/ui/OnboardingFragment.kt
+++ onboarding/src/main/kotlin/org/adblockplus/adblockplussbrowser/onboarding/ui/OnboardingFragment.kt
@@ -3,6 +3,8 @@ package org.adblockplus.adblockplussbrowser.onboarding.ui
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.addCallback
 import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,12 +39,15 @@ internal class OnboardingFragment : DataBindingFragment<FragmentOnboardingBindin
         binding.openSiButton.setOnClickListener {
             viewModel.completeOnboarding()
             try {
-                val intentLauncher = context?.packageManager?.getLaunchIntentForPackage(SBROWSER_APP_ID)
-                if (intentLauncher != null) {
-                    startActivity(intentLauncher)
-                    val intent = Intent(ACTION_OPEN_SETTINGS)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    startActivity(intent)
+                val sBrowserIntentLauncher = context?.packageManager?.getLaunchIntentForPackage(SBROWSER_APP_ID)
+                if (sBrowserIntentLauncher != null) {
+                    // Samsung browser need to be in the background in order to succeed with ACTION_OPEN_SETTINGS
+                    startActivity(sBrowserIntentLauncher)
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        val intent = Intent(ACTION_OPEN_SETTINGS)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }, START_SETTINGS_DELAY)
                 }
             } catch (e: ActivityNotFoundException) {
                 Timber.e(e)
@@ -51,7 +56,8 @@ internal class OnboardingFragment : DataBindingFragment<FragmentOnboardingBindin
     }
 
     companion object {
-        const val SBROWSER_APP_ID = "com.sec.android.app.sbrowser"
+        private const val SBROWSER_APP_ID = "com.sec.android.app.sbrowser"
+        private const val START_SETTINGS_DELAY = 500L
         private const val ACTION_OPEN_SETTINGS =
             "com.samsung.android.sbrowser.contentBlocker.ACTION_SETTING"
     }
