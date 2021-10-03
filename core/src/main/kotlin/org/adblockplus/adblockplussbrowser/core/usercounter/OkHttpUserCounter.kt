781838a594effb67845ee1acddedb6e5e3ea5afa
Krystian Zlomek <k.zlomek@eyeo.com>
2021-10-03 11:18:36 +0200

    DPC-478 - Improve user counting
    Refactoring.
    
    Signed-off-by: Krystian Zlomek <k.zlomek@eyeo.com>

--- core/src/main/kotlin/org/adblockplus/adblockplussbrowser/core/usercounter/OkHttpUserCounter.kt
+++ core/src/main/kotlin/org/adblockplus/adblockplussbrowser/core/usercounter/OkHttpUserCounter.kt
@@ -9,8 +9,10 @@ import org.adblockplus.adblockplussbrowser.base.data.model.Subscription
 import org.adblockplus.adblockplussbrowser.core.AppInfo
 import org.adblockplus.adblockplussbrowser.core.data.CoreRepository
 import org.adblockplus.adblockplussbrowser.core.extensions.currentData
+import org.adblockplus.adblockplussbrowser.core.extensions.currentSettings
 import org.adblockplus.adblockplussbrowser.core.extensions.sanatizeUrl
 import org.adblockplus.adblockplussbrowser.core.retryIO
+import org.adblockplus.adblockplussbrowser.settings.data.SettingsRepository
 import ru.gildor.coroutines.okhttp.await
 import timber.log.Timber
 import java.text.ParseException
@@ -25,11 +27,11 @@ import kotlin.time.ExperimentalTime
 internal class OkHttpUserCounter(
     private val okHttpClient: OkHttpClient,
     private val repository: CoreRepository,
+    private val settings: SettingsRepository,
     private val appInfo: AppInfo
 ) : UserCounter {
 
-    override suspend fun count(subscription: Subscription,
-                               acceptableAdsEnabled: Boolean)
+    override suspend fun count()
     : CountUserResult = coroutineScope {
         try {
             val lastVersion = repository.currentData().lastVersion
@@ -38,12 +40,15 @@ internal class OkHttpUserCounter(
             val lastVersionFormat = SimpleDateFormat("yyyyMMddHHmm", Locale.ENGLISH)
             if (lastVersion != 0L && wasSentToday(lastVersion, lastVersionFormat)) {
                 Timber.d("User count request skipped because it has been already sent today")
-                return@coroutineScope CountUserResult.Success()
+                return@coroutineScope CountUserResult.Skipped()
             }
 
-            val url = createUrl(subscription, acceptableAdsEnabled, lastVersion.toString())
+            val acceptableAdsEnabled = settings.currentSettings().acceptableAdsEnabled
+            val acceptableAdsSubscription = settings.getAcceptableAdsSubscription()
+            val url = createUrl(acceptableAdsSubscription, acceptableAdsEnabled,
+                lastVersion.toString())
             val request = Request.Builder().url(url).head().build()
-            val response = retryIO(description = subscription.title) {
+            val response = retryIO(description = "User counting") {
                 okHttpClient.newCall(request).await()
             }
 
@@ -52,7 +57,8 @@ internal class OkHttpUserCounter(
                     lastVersionFormat.timeZone = TimeZone.getTimeZone("GMT")
                     val timestamp = try {
                         // Expected date format: "Thu, 23 Sep 2021 17:31:01 GMT"
-                        val parser = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH)
+                        val parser = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z",
+                            Locale.ENGLISH)
                         lastVersionFormat.format(parser.parse(response.headers["Date"]))
                     } catch (ex: ParseException) {
                         Timber.e("Parsing 'Date' from header failed, using client GMT time")
--- core/src/main/kotlin/org/adblockplus/adblockplussbrowser/core/usercounter/UserCounter.kt
+++ core/src/main/kotlin/org/adblockplus/adblockplussbrowser/core/usercounter/UserCounter.kt
@@ -1,19 +1,11 @@
 package org.adblockplus.adblockplussbrowser.core.usercounter
 
-import org.adblockplus.adblockplussbrowser.base.data.model.Subscription
-
 internal interface UserCounter {
-    suspend fun count(subscription: Subscription, acceptableAdsEnabled: Boolean): CountUserResult
+    suspend fun count(): CountUserResult
 }
 
 internal sealed class CountUserResult {
     class Success : CountUserResult()
+    class Skipped : CountUserResult()
     class Failed : CountUserResult()
-
-    fun isSuccessful(): Boolean {
-        return when(this) {
-            is Success -> true
-            is Failed -> false
-        }
-    }
 }
