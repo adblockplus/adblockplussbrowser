573cb7e413e71dd17c504faec29842f4611d88c0
Boris Papevis <b.papevis@eyeo.com>
2021-10-07 13:59:45 +0300

    DPC-483
    Add analytics events for Languages card.

--- preferences/src/main/kotlin/org/adblockplus/adblockplussbrowser/preferences/ui/MainPreferencesViewModel.kt
+++ preferences/src/main/kotlin/org/adblockplus/adblockplussbrowser/preferences/ui/MainPreferencesViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import org.adblockplus.adblockplussbrowser.analytics.AnalyticsEvent.LANGUAGES_CARD_ADD
+import org.adblockplus.adblockplussbrowser.analytics.AnalyticsEvent.LANGUAGES_CARD_NO
 import org.adblockplus.adblockplussbrowser.analytics.AnalyticsProvider
 import org.adblockplus.adblockplussbrowser.settings.data.SettingsRepository
 import javax.inject.Inject
@@ -44,9 +46,10 @@ internal class MainPreferencesViewModel @Inject constructor(
         }
     }
 
-    fun markLanguagesOnboardingComplete() {
+    fun markLanguagesOnboardingComplete(wentAdding: Boolean) {
         viewModelScope.launch {
             settingsRepository.markLanguagesOnboardingCompleted()
         }
+        analyticsProvider.logEvent(if (wentAdding) LANGUAGES_CARD_ADD else LANGUAGES_CARD_NO)
     }
 }
