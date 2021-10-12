c43ebea8c3a40f482d0c9903fee2050f9b3aa6a8
Jacek Tymicki <j.tymicki@eyeo.com>
2021-10-12 14:02:42 +0200

    DPC-532 - Track events with AA status with Analytics
    Add user property for AA status reporting

--- analytics/src/main/java/org/adblockplus/adblockplussbrowser/analytics/AnalyticsUserProperty.kt
+++ analytics/src/main/java/org/adblockplus/adblockplussbrowser/analytics/AnalyticsUserProperty.kt
@@ -2,7 +2,7 @@ package org.adblockplus.adblockplussbrowser.analytics
 
 enum class AnalyticsUserProperty(val propertyName: String) {
     /**
-     * This user property is used to create an audience based on AA status. On every request of filter list
+     * This user property is used to create an audience based on AA status. On every filter list request
      * current status of AA is reported.
      */
     AA_STATUS("aa_status")
