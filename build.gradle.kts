49f4d24d290730cfed6eaa7b01e8594775ba6311
Boris Papevis <b.papevis@eyeo.com>
2021-10-13 10:41:54 +0300

    DPC-522 - Add copyright to new code
    Add detekt header check rule and template. Add copyright to all Kotlin and KotlinScript files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 buildscript {
     repositories {
         google()
@@ -32,7 +49,8 @@ allprojects {
 detekt {
     source = files(projectDir)
     parallel = true
-    baseline = file("${projectDir}/detekt-baseline.xml")
+    config = files("${projectDir}/config/detekt/detekt.yml")
+    baseline = file("${projectDir}/config/detekt/detekt-baseline.xml")
 
     reports {
         html.enabled = true
