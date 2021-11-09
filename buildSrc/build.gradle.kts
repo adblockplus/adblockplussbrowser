9f7b8704ad505e4741e55a2870fca9d09de8e5fa
Jacek Tymicki <j.tymicki@eyeo.com>
2021-11-09 14:08:17 +0100

    No-issue
    fix ci

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -26,5 +26,5 @@ plugins {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.2.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
 }
