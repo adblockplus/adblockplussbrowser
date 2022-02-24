c36812032a1f1cca3ecd274fd2c4f692f383a6fc
Bianca Sozzi <b.sozzi@eyeo.com>
2022-02-24 15:58:24 +0100

    WIP: DPC-701 - unit tests for subscriptions worker

--- core/src/androidTest/kotlin/org/adblockplus/adblockplussbrowser/core/work/UpdateSubscriptionsWorkerTest.kt
+++ core/src/androidTest/kotlin/org/adblockplus/adblockplussbrowser/core/work/UpdateSubscriptionsWorkerTest.kt
@@ -6,13 +6,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.work.ListenableWorker
 import androidx.work.testing.TestListenableWorkerBuilder
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.core.Is.`is`
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.Is
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 class UpdateSubscriptionsWorkerTest {
@@ -29,9 +28,9 @@ class UpdateSubscriptionsWorkerTest {
             .setTags(mutableListOf(UpdateSubscriptionsWorker.UPDATE_KEY_FORCE_REFRESH))
             .build()
         runBlocking {
-            `when`(updateSubscriptionsWorker.doWork()).thenThrow(Exception::class.java)
+            Mockito.`when`(updateSubscriptionsWorker.doWork()).thenThrow(Exception::class.java)
             val result = updateSubscriptionsWorker.doWork()
-            assertThat(result, `is`(ListenableWorker.Result.failure()))
+            MatcherAssert.assertThat(result, Is.`is`(ListenableWorker.Result.failure()))
         }
     }
 
