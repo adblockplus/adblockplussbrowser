d81de4e094062fefd958cd1a55f29f05389df384
Bianca Sozzi <b.sozzi@eyeo.com>
2022-03-07 15:33:32 +0100

    DPC-701: add Jacoco plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -143,11 +143,37 @@ tasks.register("downloadSubscriptions") {
         tasks.getByName("checkSubscriptionsFiles").run{}
     }
 }
+
+tasks.withType(Test::class.java) {
+    configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+    finalizedBy(tasks.getByName("jacocoTestReport"))
+}
+
+
 tasks.register("jacocoTestReport", JacocoReport::class.java) {
-    dependsOn(tasks.getByName("test"))
+    val coverageSourceDirs = listOf("src/main/kotlin")
+    val fileFilter = listOf(
+        "**/R.class",
+        "**/R$*.class",
+        "**/BuildConfig.*",
+        "**/Manifest*.*",
+        "**/*Test*.*",
+        "android/**/*.*"
+    )
+
+    val javaClasses = fileTree("$buildDir/intermediates/javac/worldAbpDebug/classes").setExcludes(fileFilter)
+    val kotlinClasses = fileTree("$buildDir/tmp/kotlin-classes/worldAbpDebug").setExcludes(fileFilter)
+    classDirectories.setFrom(files(javaClasses, kotlinClasses))
+    additionalSourceDirs.setFrom(files(coverageSourceDirs))
+    sourceDirectories.setFrom(files(coverageSourceDirs))
+    executionData.setFrom(
+        fileTree("$buildDir").setIncludes(listOf("jacoco/testWorldAbpDebugUnitTest.exec"))
+    )
+
     reports {
-        xml.required.set(false)
-        csv.required.set(false)
         html.required.set(true)
     }
 }
