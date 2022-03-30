530d0636afa85941dbed7222f1e8b52fd3da1616
Jacek Tymicki <j.tymicki@eyeo.com>
2022-03-30 14:46:04 +0000

    DPC-724: Non-translated elements on "More blocking options" screen are not changing in case of language change event

--- preferences/src/main/kotlin/org/adblockplus/adblockplussbrowser/preferences/ui/othersubscriptions/OtherSubscriptionsItem.kt
+++ preferences/src/main/kotlin/org/adblockplus/adblockplussbrowser/preferences/ui/othersubscriptions/OtherSubscriptionsItem.kt
@@ -17,14 +17,11 @@
 
 package org.adblockplus.adblockplussbrowser.preferences.ui.othersubscriptions
 
-import androidx.annotation.StringRes
 import org.adblockplus.adblockplussbrowser.base.data.model.Subscription
 import org.adblockplus.adblockplussbrowser.preferences.ui.GroupItemLayout
 import java.io.Serializable
 
 internal sealed class OtherSubscriptionsItem(val id: String) {
-
-    data class HeaderItem(@StringRes val titleResId: Int) : OtherSubscriptionsItem(titleResId.toString())
 
     data class DefaultItem(val subscription: Subscription, val layout: GroupItemLayout, val active: Boolean) :
         OtherSubscriptionsItem(subscription.url)
