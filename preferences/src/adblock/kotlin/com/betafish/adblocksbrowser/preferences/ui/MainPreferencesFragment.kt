86005daec08140f8d7e8797a43e0d7a064a367d2
Jacek Tymicki <j.tymicki@eyeo.com>
2022-05-19 11:36:24 +0200

    Custom kotlin example diffrent class for flavor

---
+++ preferences/src/adblock/kotlin/com/betafish/adblocksbrowser/preferences/ui/MainPreferencesFragment.kt
@@ -0,0 +1,108 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.betafish.adblocksbrowser.preferences.ui
+
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import org.adblockplus.adblockplussbrowser.base.databinding.DataBindingFragment
+import org.adblockplus.adblockplussbrowser.base.view.setDebounceOnClickListener
+import org.adblockplus.adblockplussbrowser.preferences.BuildConfig
+import org.adblockplus.adblockplussbrowser.preferences.R
+import org.adblockplus.adblockplussbrowser.preferences.databinding.FragmentMainPreferencesBinding
+
+@AndroidEntryPoint
+internal class MainPreferencesFragment :
+    DataBindingFragment<FragmentMainPreferencesBinding>(R.layout.fragment_main_preferences) {
+
+    private val viewModel: MainPreferencesViewModel by viewModels()
+
+    override fun onBindView(binding: FragmentMainPreferencesBinding) {
+        binding.viewModel = viewModel
+        val supportActionBar = (activity as AppCompatActivity).supportActionBar
+        supportActionBar?.subtitle = getString(R.string.app_subtitle)
+
+        val lifecycleOwner = this.viewLifecycleOwner
+
+        binding.mainPreferencesAdBlockingInclude.mainPreferencesPrimarySubscriptions.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToPrimarySubscriptionsFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+        binding.mainPreferencesAdBlockingInclude.mainPreferencesOtherSubscriptions.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToOtherSubscriptionsFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+        binding.mainPreferencesAdBlockingInclude.mainPreferencesAllowlist.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToAllowlistFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+        binding.mainPreferencesAdBlockingInclude.mainPreferencesUpdateSubscriptions.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToUpdateSubscriptionsFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+        binding.mainPreferencesLanguagesOnboardingInclude.mainPreferencesLanguagesOnboardingOptionAdd.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            viewModel.markLanguagesOnboardingComplete(true)
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToPrimarySubscriptionsFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+        binding.mainPreferencesLanguagesOnboardingInclude.mainPreferencesLanguagesOnboardingOptionSkip.setDebounceOnClickListener ({
+            viewModel.markLanguagesOnboardingComplete(false)
+        }, lifecycleOwner)
+        binding.mainPreferencesAcceptableAdsInclude.mainPreferencesAcceptableAds.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToAcceptableAdsFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+
+        if (BuildConfig.FLAVOR_product == BuildConfig.FLAVOR_ABP) {
+            binding.mainPreferencesReportIssueInclude.mainPreferencesReportIssue.setDebounceOnClickListener ({
+                supportActionBar?.subtitle = null
+                val direction = MainPreferencesFragmentDirections
+                    .actionMainPreferencesFragmentToReportIssueFragment()
+                findNavController().navigate(direction)
+            }, lifecycleOwner)
+        } else {
+            binding.mainPreferencesReportIssueInclude.mainPreferencesReportIssue.visibility = View.GONE
+        }
+
+        binding.mainPreferencesAboutInclude.mainPreferencesAbout.setDebounceOnClickListener ({
+            supportActionBar?.subtitle = null
+            val direction = MainPreferencesFragmentDirections
+                .actionMainPreferencesFragmentToAboutFragment()
+            findNavController().navigate(direction)
+        }, lifecycleOwner)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.checkLanguagesOnboarding()
+    }
+}
---
+++ preferences/src/adblock/kotlin/com/betafish/adblocksbrowser/preferences/ui/MainPreferencesViewModel.kt
@@ -0,0 +1,78 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.betafish.adblocksbrowser.preferences.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import org.adblockplus.adblockplussbrowser.analytics.AnalyticsEvent.LANGUAGES_CARD_ADD
+import org.adblockplus.adblockplussbrowser.analytics.AnalyticsEvent.LANGUAGES_CARD_NO
+import org.adblockplus.adblockplussbrowser.analytics.AnalyticsProvider
+import org.adblockplus.adblockplussbrowser.settings.data.SettingsRepository
+import javax.inject.Inject
+
+@HiltViewModel
+internal class MainPreferencesViewModel @Inject constructor(
+    val settingsRepository: SettingsRepository,
+) : ViewModel() {
+    @Inject
+    lateinit var analyticsProvider: AnalyticsProvider
+
+    val languagesOnboardingCompleted = settingsRepository.settings.map { settings ->
+        settings.languagesOnboardingCompleted
+    }.asLiveData()
+
+    val acceptableAdsEnabled = settingsRepository.settings.map { settings ->
+        settings.acceptableAdsEnabled
+    }.asLiveData()
+
+    val shareEvents = settingsRepository.settings.map { settings ->
+        settings.analyticsEnabled
+    }.asLiveData()
+
+    fun toggleAnalytics() {
+        viewModelScope.launch {
+            when (shareEvents.value) {
+                true -> {
+                    analyticsProvider.disable()
+                    settingsRepository.setAnalyticsEnabled(false)
+                }
+                false -> {
+                    analyticsProvider.enable()
+                    settingsRepository.setAnalyticsEnabled(true)
+                }
+            }
+        }
+    }
+
+    fun markLanguagesOnboardingComplete(wentAdding: Boolean) {
+        viewModelScope.launch {
+            settingsRepository.markLanguagesOnboardingCompleted()
+        }
+        analyticsProvider.logEvent(if (wentAdding) LANGUAGES_CARD_ADD else LANGUAGES_CARD_NO)
+    }
+
+    fun checkLanguagesOnboarding() {
+        viewModelScope.launch {
+            settingsRepository.checkLanguagesOnboardingCompleted()
+        }
+    }
+}
