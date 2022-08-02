a348170c5f2cef86214b661e94cb275b601bc85b
Bianca Sozzi <b.sozzi@eyeo.com>
2022-08-02 11:08:54 +0200

    DPC-868: ReportIssueViewModelTest.kt WIP

--- preferences/src/test/kotlin/org/adblockplus/adblockplussbrowser/preferences/helpers/Fakes.kt
+++ preferences/src/test/kotlin/org/adblockplus/adblockplussbrowser/preferences/helpers/Fakes.kt
@@ -1,11 +1,23 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.adblockplus.adblockplussbrowser.preferences.helpers
 
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
 import org.adblockplus.adblockplussbrowser.preferences.data.model.ReportIssueData
-import org.mockito.Mockito
 
 class Fakes {
 
