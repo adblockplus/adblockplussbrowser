dab2caab5ae2206b0ffe9dbe33c4675e7c65ffa4
Bianca Sozzi <b.sozzi@eyeo.com>
2022-10-21 13:56:31 +0200

    KRK-76: Fakes refactoring

--- app/src/test/kotlin/org/adblockplus/adblockplussbrowser/app/ui/helpers/Fakes.kt
+++ app/src/test/kotlin/org/adblockplus/adblockplussbrowser/app/ui/helpers/Fakes.kt
@@ -14,18 +14,22 @@
  * You should have received a copy of the GNU General Public License
  * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
  */
+@file:Suppress("EmptyFunctionBlock")
 
 package org.adblockplus.adblockplussbrowser.app.ui.helpers
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import org.adblockplus.adblockplussbrowser.base.SubscriptionsManager
+import org.adblockplus.adblockplussbrowser.base.data.model.Subscription
+import org.adblockplus.adblockplussbrowser.base.data.model.SubscriptionUpdateStatus
 import org.adblockplus.adblockplussbrowser.base.data.prefs.AppPreferences
 
-@SuppressWarnings("EmptyFunctionBlock")
 internal class CustomFakeAppPreferences(
     private val customOnBoardingCompleted: Boolean = true,
     private val customLastFilterListRequest: Long = 0L,
-    private val customReferrerAlreadyChecked: Boolean = false
+    private val customReferrerAlreadyChecked: Boolean = false,
+    private val customIsAdblockEnabled: Boolean = true
 ): AppPreferences {
     var referrerChecked = false
     override val referrerAlreadyChecked: Boolean
@@ -44,7 +48,7 @@ internal class CustomFakeAppPreferences(
         get() = flowOf(customLastFilterListRequest)
 
     override suspend fun isAdblockEnabled(): Flow<Boolean> {
-        return super.isAdblockEnabled()
+        return flowOf(customIsAdblockEnabled)
     }
 
     override suspend fun updateLastFilterRequest(lastFilterListRequest: Long) {}
@@ -53,4 +57,27 @@ internal class CustomFakeAppPreferences(
         get() = flowOf(false)
 
     override fun initialTestPagesConfigurationCompleted() {}
+}
+
+internal class FakeSubscriptionsManager(
+    private val customStatus: SubscriptionUpdateStatus = SubscriptionUpdateStatus.None,
+    private val customLastUpdate: Long = 0L
+): SubscriptionsManager {
+    var forceSubscriptionsManager = false
+
+    override val status: Flow<SubscriptionUpdateStatus>
+        get() = flowOf(customStatus)
+
+    override val lastUpdate: Flow<Long>
+        get() = flowOf(customLastUpdate)
+
+    override fun initialize() {}
+
+    override fun scheduleImmediate(force: Boolean) {
+        forceSubscriptionsManager = force
+    }
+
+    override suspend fun validateSubscription(subscription: Subscription): Boolean = false
+
+    override suspend fun updateStatus(status: SubscriptionUpdateStatus) {}
 }
