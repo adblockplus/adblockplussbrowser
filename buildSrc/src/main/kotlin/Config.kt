330b256fe079c6a72d619ac422e8d3e1dcafbc0d
Jacek Tymicki <j.tymicki@eyeo.com>
2022-11-16 23:14:30 +0100

    KRK-112 - Release Crystal 2.6.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -21,11 +21,12 @@ object Config {
 
         const val ADBLOCK = "3.3.0"
 
-        const val CRYSTAL = "2.5.0"
+        const val CRYSTAL = "2.6.0"
     }
 
     const val COMPILE_SDK_VERSION = 31
     const val BUILD_TOOLS_VERSION = "30.0.3"
+
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 31
 
