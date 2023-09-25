679388b74fe13c293274ed2d31150f94451a6f35
Jacek Tymicki <j.tymicki@eyeo.com>
2023-09-25 09:13:28 +0200

    KRK-601 Create additional tests for telemetry module
    test prepare active ping payload

---
+++ telemetry/src/test/kotlin/org/adblockplus/adblockplussbrowser/telemetry/ActivePingReporterTest.kt
@@ -0,0 +1,68 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.adblockplus.adblockplussbrowser.telemetry
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import androidx.test.core.app.ApplicationProvider
+import kotlinx.coroutines.runBlocking
+import org.adblockplus.adblockplusbrowser.testutils.FakeSettingsRepository
+import org.adblockplus.adblockplussbrowser.base.os.AppInfo
+import org.adblockplus.adblockplussbrowser.telemetry.data.DataStoreTelemetryRepository
+import org.adblockplus.adblockplussbrowser.telemetry.data.datastore.TelemetryDataSerializer
+import org.adblockplus.adblockplussbrowser.telemetry.data.proto.TelemetryData
+import org.adblockplus.adblockplussbrowser.telemetry.reporters.ActivePingReporter
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ActivePingReporterTest {
+    private val testContext: Context = ApplicationProvider.getApplicationContext()
+    private val telemetryDataStore: DataStore<TelemetryData> =
+        DataStoreFactory.create(
+            produceFile = { testContext.dataStoreFile("telemetry.pb") },
+            serializer = TelemetryDataSerializer
+        )
+    private val dataStoreTelemetryRepository: DataStoreTelemetryRepository =
+        DataStoreTelemetryRepository(telemetryDataStore)
+    private val settingsRepository = FakeSettingsRepository("")
+
+    private lateinit var activePingReporter: ActivePingReporter
+
+    @Before
+    fun setUp() {
+        activePingReporter = ActivePingReporter(
+            dataStoreTelemetryRepository,
+            settingsRepository,
+            AppInfo(),
+        )
+    }
+
+    @Test
+    fun `test prepare payload`() {
+        runBlocking {
+            val result = activePingReporter.preparePayload()
+            assertTrue(result.isSuccess)
+        }
+    }
+}
