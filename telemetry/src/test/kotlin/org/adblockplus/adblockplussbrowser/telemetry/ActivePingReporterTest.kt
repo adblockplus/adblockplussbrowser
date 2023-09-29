fa8d592b8d42823d6af05db6feb7aead7856d699
Jacek Tymicki <j.tymicki@eyeo.com>
2023-09-29 11:24:47 +0200

    KRK-601 - Create additional tests for telemetry module

---
+++ telemetry/src/test/kotlin/org/adblockplus/adblockplussbrowser/telemetry/ActivePingReporterTest.kt
@@ -0,0 +1,113 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.adblockplus.adblockplussbrowser.telemetry
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import androidx.test.core.app.ApplicationProvider
+import androidx.work.Data
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.adblockplus.adblockplusbrowser.testutils.FakeSettingsRepository
+import org.adblockplus.adblockplussbrowser.base.os.AppInfo
+import org.adblockplus.adblockplussbrowser.telemetry.data.DataStoreTelemetryRepository
+import org.adblockplus.adblockplussbrowser.telemetry.data.datastore.TelemetryDataSerializer
+import org.adblockplus.adblockplussbrowser.telemetry.data.proto.TelemetryData
+import org.adblockplus.adblockplussbrowser.telemetry.reporters.ActivePingReporter
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+
+@Serializable
+data class Payload(
+    @SerialName("last_ping_tag") val lastPingTag: String,
+    @SerialName("first_ping") val firstPing: String,
+    @SerialName("application") val application: String,
+    @SerialName("application_version") val applicationVersion: String,
+    @SerialName("aa_active") val aaActive: Boolean,
+    @SerialName("platform") val platform: String,
+    @SerialName("platform_version") val platformVersion: String,
+    @SerialName("extension_name") val extensionName: String,
+    @SerialName("extension_version") val extensionVersion: String
+)
+
+@Serializable
+data class ActivePing(
+    var payload: Payload
+)
+
+@RunWith(RobolectricTestRunner::class)
+class ActivePingReporterTest {
+    private val testContext: Context = ApplicationProvider.getApplicationContext()
+    private val telemetryDataStore: DataStore<TelemetryData> =
+        DataStoreFactory.create(
+            produceFile = { testContext.dataStoreFile("telemetry.pb") },
+            serializer = TelemetryDataSerializer
+        )
+    private val dataStoreTelemetryRepository: DataStoreTelemetryRepository =
+        DataStoreTelemetryRepository(telemetryDataStore)
+    private val settingsRepository = FakeSettingsRepository("")
+
+    private lateinit var activePingReporter: ActivePingReporter
+
+    @Before
+    fun setUp() {
+        runBlocking {
+            val testFirstPing = 1692845403742
+            dataStoreTelemetryRepository.updateFirstPingIfNotSet(testFirstPing)
+        }
+        activePingReporter = ActivePingReporter(
+            dataStoreTelemetryRepository,
+            settingsRepository,
+            AppInfo(),
+        )
+    }
+
+    @Test
+    fun `test prepare payload`() {
+        runBlocking {
+            val result = activePingReporter.preparePayload()
+            assertTrue(result.isSuccess)
+            result.onSuccess { activePingPayload ->
+                val json = Json { ignoreUnknownKeys = true }
+                val activePing = json.decodeFromString<ActivePing>(activePingPayload)
+                assertTrue(activePing.payload.aaActive)
+                assertEquals("2023-08-24T02:50:03.742Z", activePing.payload.firstPing)
+                assertEquals("android", activePing.payload.platform)
+            }
+        }
+    }
+
+    @Test
+    fun `test process response`() {
+        runBlocking {
+            val response = Data.Builder().putString("token", "2023-09-26T14:30:00Z").build()
+            val processedResponse = activePingReporter.processResponse(response)
+            assertTrue(processedResponse.isSuccess)
+        }
+    }
+}
---
+++ telemetry/src/test/kotlin/org/adblockplus/adblockplussbrowser/telemetry/TelemetryRepositoryTest.kt
@@ -0,0 +1,77 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.adblockplus.adblockplussbrowser.telemetry
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import androidx.test.core.app.ApplicationProvider
+import kotlinx.coroutines.runBlocking
+import org.adblockplus.adblockplussbrowser.telemetry.data.DataStoreTelemetryRepository
+import org.adblockplus.adblockplussbrowser.telemetry.data.datastore.TelemetryDataSerializer
+import org.adblockplus.adblockplussbrowser.telemetry.data.proto.TelemetryData
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner::class)
+class TelemetryRepositoryTest {
+    private val testContext: Context = ApplicationProvider.getApplicationContext()
+
+    private val telemetryDataStore: DataStore<TelemetryData> =
+        DataStoreFactory.create(
+            produceFile = { testContext.dataStoreFile("telemetry.pb") },
+            serializer = TelemetryDataSerializer
+        )
+
+    private val dataStoreTelemetryRepository: DataStoreTelemetryRepository =
+        DataStoreTelemetryRepository(telemetryDataStore)
+
+    @Test
+    fun `test data store init`() {
+        runBlocking {
+            assertTrue(dataStoreTelemetryRepository.currentData().isInitialized)
+        }
+    }
+
+    @Test
+    fun `test update first ping`() {
+        runBlocking {
+            val testFirstPing = 1692845403742
+            dataStoreTelemetryRepository.updateFirstPingIfNotSet(testFirstPing)
+            assertEquals(testFirstPing, dataStoreTelemetryRepository.currentData().firstPing)
+        }
+    }
+
+    @Test
+    fun `test update and shift lastPing to previousLast`() {
+        runBlocking {
+            val testNewLastPing = 1692845403742
+            dataStoreTelemetryRepository.updateAndShiftLastPingToPreviousLast(1692845403742)
+            val testSecondNewLastPing = 1692845403743
+            dataStoreTelemetryRepository.updateAndShiftLastPingToPreviousLast(testSecondNewLastPing)
+            assertEquals(testSecondNewLastPing, dataStoreTelemetryRepository.currentData().lastPing)
+            assertEquals(testNewLastPing, dataStoreTelemetryRepository.currentData().previousLastPing)
+        }
+    }
+}
