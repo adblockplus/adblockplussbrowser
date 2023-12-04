a184b62dd9c006646e607b0a9ba2194f36ee6ff6
Jacek Tymicki <j.tymicki@eyeo.com>
2023-12-04 16:36:41 +0100

    KRK-736 - Release Adblock 3.4.5

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -19,7 +19,7 @@ object Config {
     object Versions {
         const val ABP = "2.5.4"
 
-        const val ADBLOCK = "3.4.4"
+        const val ADBLOCK = "3.4.5"
 
         const val CRYSTAL = "2.6.5"
     }
