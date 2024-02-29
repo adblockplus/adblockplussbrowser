8663a3bcd1f4aeac97cf50227ab4ef79da755307
Jacek Tymicki <j.tymicki@eyeo.com>
2024-02-29 18:44:40 +0100

    KRK-764 - Investigate if wake lock issue with content blockers
    remove unused extensions

--- core/src/main/kotlin/org/adblockplus/adblockplussbrowser/core/extensions/WorkExtensions.kt
+++ core/src/main/kotlin/org/adblockplus/adblockplussbrowser/core/extensions/WorkExtensions.kt
@@ -27,33 +27,6 @@ import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
-internal inline fun <reified W : ListenableWorker> periodicWorkRequestBuilder(
-    repeatInterval: Duration,
-    flexInterval: Duration
-): PeriodicWorkRequest.Builder {
-    repeatInterval.inWholeMinutes
-    return PeriodicWorkRequest.Builder(W::class.java, repeatInterval.inWholeMinutes, TimeUnit.MINUTES,
-        flexInterval.inWholeMinutes, TimeUnit.MINUTES)
-}
-
-@ExperimentalTime
-internal fun WorkRequest.Builder<PeriodicWorkRequest.Builder, PeriodicWorkRequest>.setInitialDelay(
-    duration: Duration
-): WorkRequest.Builder<PeriodicWorkRequest.Builder, PeriodicWorkRequest> {
-    this.setInitialDelay(duration.inWholeMinutes, TimeUnit.MINUTES)
-    return this
-}
-
-@ExperimentalTime
-internal fun WorkRequest.Builder<PeriodicWorkRequest.Builder, PeriodicWorkRequest>.setBackoffCriteria(
-    policy: BackoffPolicy,
-    duration: Duration
-): WorkRequest.Builder<PeriodicWorkRequest.Builder, PeriodicWorkRequest> {
-    this.setBackoffCriteria(policy, duration.inWholeSeconds, TimeUnit.SECONDS)
-    return this
-}
-
-@ExperimentalTime
 internal fun WorkRequest.Builder<OneTimeWorkRequest.Builder, OneTimeWorkRequest>.setBackoffTime(
     duration: Duration
 ): WorkRequest.Builder<OneTimeWorkRequest.Builder, OneTimeWorkRequest> {
